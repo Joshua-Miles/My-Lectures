b4ace31b8351ad668a3d506fe88a951dc062198c
joshua-miles <joshua@christetulit.org>
2020-04-01 17:05:08 -0500

    -

--- 19-web-applications/app/controllers/application_controller.rb
+++ 19-web-applications/app/controllers/application_controller.rb
@@ -3,18 +3,8 @@ class ApplicationController < Sinatra::Base
     set(:views, './app/views')
 
    
-    get('/pantry') do 
-        @items = PantryItem.all # params
-        erb(:item_list)
-    end
-
-    post('/add-item') do 
-        PantryItem.create({
-            name: params[:name],
-            category: params[:category],
-            quantity: params[:quantity]
-        })
-        redirect '/pantry'
+    get('/') do 
+        "Hello World"
     end
 
 end
